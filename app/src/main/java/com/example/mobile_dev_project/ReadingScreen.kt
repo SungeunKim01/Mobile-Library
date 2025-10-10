96cff34551d564f9e7d915d11e5baf7aeabd5e35
Dorian Castillo <=>
2025-10-10 18:03:20 -0400

    Add mock chapters.

---
+++ app/src/main/java/com/example/mobile_dev_project/ReadingScreen.kt
@@ -0,0 +1,216 @@
+package com.example.mobile_dev_project
+
+import android.app.Activity
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Text
+import androidx.compose.material3.Button
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+
+
+//pairs: https://kotlinlang.org/api/core/kotlin-stdlib/kotlin/-pair/
+@Composable
+fun ReadingScreen (chapters: List<Pair<String, String>>,
+                   chapterIndexSelected: Int,
+                   onSearch: () -> Unit) {
+    var currentChapterIndex by remember { mutableStateOf(chapterIndexSelected) }
+}
+
+// Flowers for Algernon (Progress Reports 1–7)
+val mockChapters = listOf(
+    "progris riport l" to """
+        martch 5 1965
+        Dr. Strauss says I should write down what I think and remember. 
+        He says it’s like keeping a diary. I hope they use me for the experiment. 
+        Miss Kinnian says maybe they can make me smart. I want to be smart. My
+        name is Charlie Gordon. I am 37 years old and 2 weeks ago was my brithday.
+        I have nuthing more to rite now so I will close for today
+    """.trimIndent(),
+
+    "progris riport 2" to """
+        martch 6
+        I had a test today. I think I faled it. and I think that maybe now they wont
+        use me. What happind is a nice young man was in the room and he had some
+        white cards with ink spillled all over them. He sed Charlie what do you see
+        on this card. I was very skared even tho I had my rabits foot in my pockit
+        because when I was a kid I always faled tests in school and I spillled ink to.
+        I told him I saw a inkblot. He said yes and it made me feel good. I thot
+        that was all but when I got up to go he stopped me. He said now sit down
+        Charlie we are not thru yet. Then I dont remember so good but he wan tid me
+        to say what was in the ink. I dint see nuthing in the ink but he said there was
+        picturs there other pepul saw some picturs. I coudnt see any picturs. I reely
+        tryed to see. I held the card close up and then far away. Then I said if 1 had
+        my glases I coud see better I usally only ware my glases in the movies or TV
+        but 1 said they are in the closh in the hall. I got them. Then I said let me see
+        that card agen I bet III find it now.
+        I tryed hard but I still coudnt find the picturs I only saw the ink. 1 told
+        him maybe 1 need new glases. He rote somthing down on a paper and I got
+        skared of faling the test. I told him it was a very nice inkblot with littel points
+        all around the eges. He looked very sad so that wasnt it. I said please let me
+        try agen. III get it in a few minits becaus 1m not so fast somtimes. 1m a slow
+        reeder too in Miss Kinnians class for slow adults but I'm trying very hard.
+        He gave me a chance with another card that had 2 kinds of ink spilled
+        on it red and blue.
+        He was very nice and talked slow like Miss Kinnian does and he ex
+        planed it to me that it was a raw shok. He said pepul see things in the ink.
+        I said show me where. He said think. I told him I think a inkblot but that
+        wasnt rite eather. He said what does it remind you-pretend something. 1
+        closd my eyes for a long time to pretend. I told him I pretend a fowntan pen
+        with ink leeking all over a table cloth. Then he got up and went out.
+        1 dont think I passd the raw shok test.
+    """.trimIndent(),
+
+    "progris report 3" to """
+        martch 7
+        Dr Strauss and Dr Nemur say it dont matter about the inkblots. I told them 
+        I dint spill the ink on the cards and I coudnt see anything in the ink. They said 
+        that maybe they will still use me. I said Miss Kinnian never gave me tests like
+        that one only spelling and reading. They said Miss Kinnian told that I was her
+        bestist pupil in the adult nite scool becaus I tryed the hardist and I reely
+        wantid to lern. They said how come you went to the adult nite scool all by
+        yourself Charlie. How did you find it. I said I askd pepul and sumbody told
+        me where I shud go to lern to read and spell good. They said why did you want
+        to. I told them becaus all my life I wantid to be smart and not dumb. But its
+        very hard to be smart. They said you know it will probly be tempirery. I said
+        yes. Miss Kinnian told me. I dont care if it herts.
+        Later I had more crazy tests today. The nice lady who gave it me told
+        me the name and I asked her how do you spellit so I can rite it in my progris
+        riport. THEMATIC APPERCEPTION TEST. I dont know the frist 2 words but I know
+        what test means. You got to pass it or you get bad marks. This test lookd easy
+        becaus I coud see the picturs. Only this time she dint want me to tell her the
+        picturs. That mixd me up. I said the man yesterday said I shoud tell him what
+        I saw in the ink she said that dont make no difrence. She said make up storys
+        about the pepul in the picturs.
+        I told her how can you tell storys about pepul you never met. I said why
+        shud I make up lies. I never tell lies any more becaus I always get caut.
+        She told me this test and the other one the raw-shok was for getting
+        personalty. I laffed so hard. I said how can you get that thing from inkblots
+        and fotos. She got sore and put her picturs away. I dont care. It was siIy. I gess
+        I faled that test too.
+        Later some men in white coats took me to a difernt part of the hospitil
+        and gave me a game to play. It was like a race with a white mouse. They called
+        the mouse Algernon. Algernon was in a box with a lot of twists and turns like
+        all-kinds of walls and they gave me a pencil and a paper with lines and lots
+        of boxes. On one side it said START and on the other end it said FINISH. They
+        said it was amazed and that Algernon and me had the same amazed to do.
+        I dint see how we could have the same amazed if Algernon had a box and I
+        had a paper but I dint say nothing. Anyway there wasnt time because the race
+        started.
+        One of the men had a watch he was trying to hide so I woudnt see it so
+        I tryed not to look and that made me nervus.
+        Anyway that test made me feel worser than all the others because they
+        did it over 10 times with difernt amazeds and Algernon won every time. I dint
+        know that mice were so smart. Maybe thats because Algernon is a white
+        mouse. Maybe white mice are smarter then other mice.
+    """.trimIndent(),
+
+    "progris riport 4" to """
+        Mar 8
+        Their going to use me! 1m so exited I can hardly write. Dr Nemur and Dr
+        Strauss had a argament about it first. Dr Nemur was in the office when Dr
+        Strauss brot me in. Dr Nemur was worryed about using me but Dr Strauss
+        told him Miss Kinnian rekemmended me the best from all the people who she
+        was teaching. I like Miss Kinnian becaus shes a very smart teacher. And she
+        said Charlie your going to have a second chance. If you volenteer for this
+        experamenl you mite get smart. They dont know if it will be perminint but
+        theirs a chance. Thats why I said ok even when I was scared because she said
+        it was an operashun. She said dont be scared Charlie you done so much with
+        so little I think you deserv it most of all.
+        
+        So I got scaird when Dr Nemur and Dr Strauss argud about it. Dr Strauss
+        said I had something that was very good. He said I had a good motor-vation.
+        I never even knew I had that. I felt proud when he said that not every body
+        with an eye-q of 68 had that thing. I dont know what it is or where I got it
+        but he said Algernon had it too. Algernons motor-vation is the cheese they put
+        in his box. But it cant be that because I didnt eat any cheese this week.
+        Then he told Dr Nemur something I dint understand so while they were
+        talking I wrote down some of the words.
+        He said Dr Nemur I know Charlie is not what you had in mind as the
+        first of your new brede of intelek,Hr (coudnt get the word) superman. But most
+        people of his low menr"'* are host~d' and uncoop'~~' they are usualy dull
+        apath,'0': and hard to reach. He has a good natcher hes intristed and eager to
+        please.
+        Dr Nemur said remember he will be the first human beeng ever to have
+        his intelijence trippled by surgicle meens.
+        Dr Strauss said exakly. Look at how well hes lerned to read and write
+        for his low mentel age its as grate an acheve H as you and I lerning einstines
+        therey of **vity without help. That shows the intenss motor-vation. Its com
+        parat** a tremen** achev** I say we use Charlie.
+        I dint get all the words and they were talking to fast but it sounded like
+        Dr Strauss was on my side and like the other one wasnt.
+        Then Dr Nemur nodded he said all right maybe your right. We will use
+        Charlie. When he said that I got so exited I jumped up and shook his hand
+        for being so good to me. I told him thank you doc you wont be sorry for giving
+        me a second chance. And I mean it like I told him. After the operashun 1m
+        gonna try to be smart. Im gonna try awful hard.
+    """.trimIndent(),
+
+    "progris ript 5" to """
+        Mar 10
+        1m skared. Lots of people who work here and the nurses and the people who
+        gave me the tests came to bring me candy and wish me luck. I hope I have
+        luck. I got my rabits foot and my lucky penny and my horse shoe. Only a black
+        cat crossed me when I was comming to the hospitil. Dr Strauss says dont be
+        supersitis Charlie this is sience. Anyway 1m keeping my rabits foot with me.
+        I.asked Dr Strauss if Ill beat Algernon in the race after the operashun
+        and he said maybe. If the operashun works III show that mouse I can be as
+        smart as he is. Maybe smarter. Then III be abel to read better and spell the
+        words good and know lots of things and be like other people. I want to be
+        smart like other people. If it works perminint they will make everybody smart
+        all over the wurld.
+        They dint give me anything to eat this morning. I dont know what that
+        eating has to do with getting smart. 1m very hungry and Dr Nemur took away
+        my box of candy. That Dr Nemur is a grouch. Dr Strauss says I can have it
+        back after the operashun. You cant eat befor a operashun ...
+    """.trimIndent(),
+
+    "progress report 6" to """
+        Mar 15
+        The operashun dint hurt. He did it while I was sleeping. They took off the
+        bandijis from my eyes and my head today so I can make a PROGRESS REPORT.
+        Dr Nemur who looked at some of my other ones says I spell PROGRESS wrong
+        and he told me how to spell it and REPORT too. I got to try and remember that.
+        I have a very bad memary for spelling. Dr Strauss says its ok to tell about
+        all the things that happin to me but he says I shoud tell more about what I
+        feel and what I think. When I told him I dont know how to think he said try.
+        All the time when the bandijis were on my eyes I tryed to think. Nothing
+        happened. I dont know what to think about. Maybe if I ask him he will tell
+        me how I can think now that 1m suppose to get smart. What do smart people
+        think about. Fancy things I suppose. I wish I knew some fancy things alredy.
+    """.trimIndent(),
+
+    "progress report 7" to """
+        mar 19
+        Nothing is happining. I had lots of tests and different kinds of races with
+        Algernon. I hate that mouse. He always beats me. Dr Strauss said I got to play
+        those games. And he said some time I got to take those tests over again. Thse
+        inkblots are stupid. And those pictures are stupid too. I like to draw a picture
+        of a man and a woman but I wont make up lies about people.
+        I got a headache from trying to think so much. I that Dr Strauss was my
+        frend but he dont help me. He dont tell me what to think or when III get smart.
+        Miss Kinnian dint come to see me. I think writing these progress reports are
+        stupid too.
+    """.trimIndent()
+)
