cff0dc9c8afe0a7449c457ca8a124cf89951d007
Christian.Graceffa <christiamgraceffa@gmail.com>
2025-10-14 14:41:31 -0400

    Add Logo with title

---
+++ app/src/main/java/com/example/mobile_dev_project/screens/HomeScreen.kt
@@ -0,0 +1,68 @@
+package com.example.mobile_dev_project.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.mobile_dev_project.R
+import com.example.mobile_dev_project.ui.theme.MobileDevProjectTheme
+
+@Composable
+fun HomeScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            RestaurantLogo()
+
+            Text(
+                text = "Welcome to Mobile Library",
+                style = MaterialTheme.typography.titleLarge
+            )
+        }
+    }
+}
+    @Composable
+    fun RestaurantLogo(modifier: Modifier = Modifier.Companion) {
+        val image = painterResource(R.drawable.mobile_library)
+        Column {
+            Image(
+                painter = image,
+                contentDescription = null,
+                modifier = modifier
+                    .size(100.dp)
+                    .clip(RoundedCornerShape(16.dp))
+                    .aspectRatio(1f),
+
+                contentScale = ContentScale.Companion.Crop
+            )
+        }
+    }
+
+@Preview(showBackground = true)
+@Composable
+fun HomeScreenPreview() {
+    MobileDevProjectTheme {
+        HomeScreen()
+    }
+}
