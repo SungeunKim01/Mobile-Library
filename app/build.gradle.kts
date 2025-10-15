7a6b12abf31236323907d8736bcfead09380e898
Christian.Graceffa <christiamgraceffa@gmail.com>
2025-10-15 17:32:16 -0400

    Add Navigation to Home

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,7 +51,7 @@ dependencies {
     implementation("androidx.compose.material:material-icons-extended")
     implementation("org.jsoup:jsoup:1.21.2")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.3")
-
+    implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
--- app/src/main/java/com/example/mobile_dev_project/nav/Route.kt
+++ app/src/main/java/com/example/mobile_dev_project/nav/Route.kt
@@ -3,4 +3,5 @@ package com.example.mobile_dev_project.nav
 sealed class Route(val route: String) {
     data object Download : Route("download")
     data object Search   : Route("search")
+    data object Home : Route("Home")
 }
