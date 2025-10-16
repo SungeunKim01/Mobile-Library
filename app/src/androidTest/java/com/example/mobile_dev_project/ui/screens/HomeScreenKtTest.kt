f7c9e1c0e3d037215c4fb5ad7880ba1108d859b8
Christian.Graceffa <christiamgraceffa@gmail.com>
2025-10-16 18:25:08 -0400

    Add Navigation Tests

--- app/src/androidTest/java/com/example/mobile_dev_project/ui/screens/HomeScreenKtTest.kt
+++ app/src/androidTest/java/com/example/mobile_dev_project/ui/screens/HomeScreenKtTest.kt
@@ -1,20 +1,37 @@
 package com.example.mobile_dev_project.ui.screens
 
 import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
+import androidx.navigation.compose.rememberNavController
 import org.junit.Test
 import org.junit.runner.RunWith
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.mobile_dev_project.MainActivity
+import com.example.mobile_dev_project.ui.AppScaffold
+import com.example.mobile_dev_project.ui.BottomNavigationBar
 import com.example.mobile_dev_project.ui.theme.MobileDevProjectTheme
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.Before
 import org.junit.Rule
 
 @RunWith(AndroidJUnit4::class)
+@HiltAndroidTest
 class HomeScreenKtTest {
 
     @get:Rule
-    val composeTestRule = createComposeRule()
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
+
+    @get:Rule
+    val hiltRule = HiltAndroidRule(this)
+
+    @Before
+    fun setup() {
+        hiltRule.inject()
+    }
 
     @Test
     fun homeScreen() {
@@ -83,9 +100,10 @@ class HomeScreenKtTest {
 
     @Test
     fun downloadBookButton() {
+
         composeTestRule.setContent {
             MobileDevProjectTheme {
-                HomeScreen()
+                AppScaffold(nav = rememberNavController())
             }
         }
 
@@ -93,5 +111,18 @@ class HomeScreenKtTest {
             .assertExists()
             .assertIsDisplayed()
             .performClick()
+
+        //This is testing if the download page is shown after the click is pressed, and I dont want to change another persons code this will do
+        composeTestRule.onNodeWithTag("UrlField")
+            .assertExists()
+            .assertIsDisplayed()
+
+        composeTestRule.onNodeWithTag("AddButton")
+            .assertExists()
+            .assertIsDisplayed()
+
+        composeTestRule.onNodeWithTag("CancelButton")
+            .assertExists()
+            .assertIsDisplayed()
     }
 }
