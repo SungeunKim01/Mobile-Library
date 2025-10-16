ee93b8b673c54ddb5887cc6d7fbc99bd6464bd19
Christian.Graceffa <christiamgraceffa@gmail.com>
2025-10-16 10:30:25 -0400

    Fix Download Nagivation

--- app/src/main/java/com/example/mobile_dev_project/ui/AppNavHost.kt
+++ app/src/main/java/com/example/mobile_dev_project/ui/AppNavHost.kt
@@ -59,7 +59,7 @@ fun AppNavHost(
         // Add other screens like Home, we will merge this file after we finish all the screens
         composable(Route.Home.route) {
             HomeScreen(
-                onNavigateToDownload = { nav.popBackStack() }
+                onNavigateToDownload = { nav.navigate(Route.Download.route)  }
             )
         }
 
