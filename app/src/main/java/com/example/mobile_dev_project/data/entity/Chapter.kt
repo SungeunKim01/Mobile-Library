3c34c1b4fc10b902f9692a2c6bddfb41a9a2f445
Christian.Graceffa <christiamgraceffa@gmail.com>
2025-10-29 16:57:23 -0400

    Add Entity Code to Chapter

--- app/src/main/java/com/example/mobile_dev_project/data/entity/Chapter.kt
+++ app/src/main/java/com/example/mobile_dev_project/data/entity/Chapter.kt
@@ -1,22 +1,36 @@
 package com.example.mobile_dev_project.data.entity
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
 
-
-
+@Entity(
+    tableName = "chapters",
+    foreignKeys = [ForeignKey(
+        entity = Book::class,
+        parentColumns = ["bookId"],
+        childColumns = ["bookId"],
+        onDelete = ForeignKey.CASCADE
+    )]
+)
 class Chapter {
 
 
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "chapterId")
     var chapterId: Int = 0
 
-
+    @ColumnInfo(name = "bookId")
     var bookId: Int = 0
 
-
+    @ColumnInfo(name = "chapterName")
     var chapterName: String? = null
 
-
+    @ColumnInfo(name = "chapterOrder")
     var chapterOrder: Int = 0
 
+    @ColumnInfo(name = "contentId")
     var contentId: String? = null
 
     constructor() {}
