24127aae68b84d6a189612c51b445ad540c9d7a6
Christian.Graceffa <christiamgraceffa@gmail.com>
2025-10-29 10:20:17 -0400

    Modified to make database work

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,22 +4,26 @@ plugins {
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.hilt)
     alias(libs.plugins.kotlin.kapt)
+    id("com.google.devtools.ksp")
 }
 
 android {
     namespace = "com.example.mobile_dev_project"
-    compileSdk = 36
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.mobile_dev_project"
-        minSdk = 26
-        targetSdk = 36
+        minSdk = 30
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "com.example.mobile_dev_project.ui.screens.HiltTestRunner"
     }
-
+    ksp {
+        arg("room.schemaLocation", "$projectDir/schemas")
+        arg("room.expandProjection", "true")
+    }
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -68,6 +72,12 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation("androidx.compose.ui:ui-text-google-fonts:1.6.0")
+    implementation("androidx.room:room-runtime:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+    implementation("androidx.compose.foundation:foundation-layout-android:1.9.4")
+    annotationProcessor("androidx.room:room-compiler:2.6.1")
+    ksp("androidx.room:room-compiler:2.6.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
