d89728fbc49d5b2fe42127b3c87ddc005837c533
Christian.Graceffa <christiamgraceffa@gmail.com>
2025-10-29 09:09:09 -0400

    Add Constructor

--- app/src/main/java/com/example/mobile_dev_project/data/entity/Book.kt
+++ app/src/main/java/com/example/mobile_dev_project/data/entity/Book.kt
@@ -1,4 +1,23 @@
 package com.example.mobile_dev_project.data.entity
 
+import java.util.Date
+
 class Book {
+    var BookId: Int = 0
+    var BookTitle: String? = null
+    var LastAccessed: Date? = null
+    var DateAccessed: Date? = null
+    var BookCoverPath: String? = null
+
+    constructor() {}
+
+    constructor(BookId: Int, BookTitlel: String, LastAccessed: Date, DateAdded: Date, BookCoverPath: String) {
+        this.BookId = BookId
+        this.BookTitle = BookTitle
+        this.LastAccessed = LastAccessed
+        this.DateAccessed = DateAccessed
+        this.BookCoverPath = BookCoverPath
+    }
+
+
 }
