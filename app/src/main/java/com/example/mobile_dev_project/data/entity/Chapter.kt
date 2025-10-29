df12b1c5cc6ae44cd01025bd4cd07e47c3d1c7da
Christian.Graceffa <christiamgraceffa@gmail.com>
2025-10-29 16:52:55 -0400

    Initial info Chapter

--- app/src/main/java/com/example/mobile_dev_project/data/entity/Chapter.kt
+++ app/src/main/java/com/example/mobile_dev_project/data/entity/Chapter.kt
@@ -1,4 +1,30 @@
 package com.example.mobile_dev_project.data.entity
 
+
+
+
 class Chapter {
+
+
+    var chapterId: Int = 0
+
+
+    var bookId: Int = 0
+
+
+    var chapterName: String? = null
+
+
+    var chapterOrder: Int = 0
+
+    var contentId: String? = null
+
+    constructor() {}
+
+    constructor(bookId: Int, chapterName: String?, chapterOrder: Int, contentId: String?) {
+        this.bookId = bookId
+        this.chapterName = chapterName
+        this.chapterOrder = chapterOrder
+        this.contentId = contentId
+    }
 }
