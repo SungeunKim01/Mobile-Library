e0863a82e3dec8892a180ef856b49bba911c03bf
Christian.Graceffa <christiamgraceffa@gmail.com>
2025-10-29 20:06:13 -0400

    Initial Code for Database

--- app/src/main/java/com/example/mobile_dev_project/data/database/RoomDatabase.kt
+++ app/src/main/java/com/example/mobile_dev_project/data/database/RoomDatabase.kt
@@ -1,4 +1,23 @@
 package com.example.mobile_dev_project.data.database
 
-class RoomDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.mobile_dev_project.data.dao.BookDao
+import com.example.mobile_dev_project.data.dao.ChapterDao
+import com.example.mobile_dev_project.data.dao.ContentDao
+import com.example.mobile_dev_project.data.entity.Book
+import com.example.mobile_dev_project.data.entity.Chapter
+import com.example.mobile_dev_project.data.entity.Content
+
+@Database(
+    entities = [Book::class, Chapter::class, Content::class],
+    version = 1,
+    )
+abstract class RoomDatabase : RoomDatabase(){
+
+    abstract fun bookDao(): BookDao
+    abstract fun chapterDao(): ChapterDao
+    abstract fun contentDao(): ContentDao
 }
