ede918d8e253f0da88dfd852a7f9010d9f9ba49b
FrancisMignacca <francispatmignacca@yahoo.com>
2025-10-30 21:35:12 -0400

    Finish Book class

--- app/src/main/java/com/example/mobile_dev_project/data/Book.kt
+++ app/src/main/java/com/example/mobile_dev_project/data/Book.kt
@@ -1,12 +1,23 @@
 package com.example.mobile_dev_project.data
 
+import java.time.LocalDate
+import java.util.Date
+
 class Book {
     var chapters: List<Chapter>
     var title: String
+    var dateAdded: Date
+    var lastAccessed: Date
 
     constructor(title: String, chapters: List<Chapter>){
         this.title = title
         this.chapters = chapters
+        this.dateAdded = Date(LocalDate.now().year, LocalDate.now().monthValue, LocalDate.now().dayOfMonth)
+        this.lastAccessed = Date(LocalDate.now().year, LocalDate.now().monthValue, LocalDate.now().dayOfMonth)
+    }
+
+    fun accessedBook(){
+        this.lastAccessed = Date(LocalDate.now().year, LocalDate.now().monthValue, LocalDate.now().dayOfMonth)
     }
 }
 
