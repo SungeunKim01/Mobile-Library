38f921cae122d350999d5ca83ed4684a621311eb
Dorian Castillo <=>
2025-11-01 20:36:09 -0400

    Remove storing data and only extract data.

--- app/src/main/java/com/example/mobile_dev_project/data/repository/ParsingRepository.kt
+++ app/src/main/java/com/example/mobile_dev_project/data/repository/ParsingRepository.kt
@@ -3,7 +3,7 @@ package com.example.mobile_dev_project.data.repository
 import android.util.Log
 import java.io.File
 import javax.inject.Inject
-import com.example.mobile_dev_project.data.dao.*
+import com.example.mobile_dev_project.data.*
 import com.example.mobile_dev_project.data.entity.Book
 import com.example.mobile_dev_project.data.entity.Chapter
 import com.example.mobile_dev_project.data.entity.Content
@@ -15,87 +15,91 @@ import kotlinx.coroutines.withContext
 
 class ParsingRepository @Inject constructor(
     private val paths: BooksPaths,
-    private val bookd: BookDao,
-    private val chapterd: ChapterDao,
-    private val contentd: ContentDao
 ){
-    //Locate the html file from the created book folder upon url entry and store metadata in database.
+    //Locate the html file from the created book folder upon url entry and extract all metadata needed for storing later.
     //for the basics: https://github.com/fleeksoft/ksoup
-    suspend fun parseHtml(bookId: Int) = withContext(Dispatchers.IO){
+    suspend fun parseHtml(bookId: Long): Pair<UiBook, List<UiContent>> = withContext(Dispatchers.IO){
         try {
             //get html file
             val directory = paths.bookContentFolder(bookId.toString())
             //val htmlFile = contentFolder.listFiles()
             //    ?.firstOrNull { it.name.endsWith(".html", ignoreCase = true) }
-
             //this is better bcs on other websites, maybe html file is under nested subfolder
             //src: https://kotlinlang.org/api/core/kotlin-stdlib/kotlin.io/walk-top-down.html
             val html = directory.walkTopDown().firstOrNull {it.extension.equals("html", ignoreCase = true)}
 
             if (html == null) {
                 Log.e("ParsingRepository", "HTML file not foudn for book: $bookId.")
-                return@withContext
+                return@withContext Pair(
+                    UiBook(bookId = bookId.toInt(), title = "Untitled Book", coverPath = "", chapters = emptyList()),
+                    emptyList()
+                )
             }
-
             val htmlContent = html.readText(Charsets.UTF_8)
             val doc: Document = Ksoup.parse(htmlContent)
 
             //get the title that is inside meta tag and if doesnt exist, just get title from title tag
-            updateBookTitle(bookId, doc)
+            val bookTitle = extractBookTitle(doc)
 
             //get chapters & their content
-            storeChapterAndContentData(bookId, directory, doc)
+            val (chapters, contents) = extractChapterAndContent(bookId.toInt(), directory, doc)
 
+            //create uibook using title
+            val uiBook = UiBook(bookId.toInt(), chapters, bookTitle,"")
+            Pair(uiBook, contents)
         } catch (e: Exception){
             Log.e("ParsingRepository", "Error occurred while parsing book with id $bookId: ${e.message}")
+            Pair(
+                UiBook(bookId = bookId.toInt(), title = "Untitled Book", coverPath = "", chapters = emptyList()),
+                emptyList()
+            )
         }
 
     }
 
     /**
      * Locate the title that is inside <meta> tag with name="dc.title".
-     * Store the title in provided bookId.
+     * Extract the title.
      */
-    private suspend fun updateBookTitle(bookId: Int, doc: Document){
+    private fun extractBookTitle(doc: Document): String{
         val title = doc.selectFirst("meta[name=dc.title]")?.attr("content")?.trim()
             ?: doc.selectFirst("h1")?.text()?.trim()
             ?: "Untitled Book"
 
-        val bookToUpdate = bookd.getSingularBookById(bookId)
-        if(bookToUpdate != null) {
-            bookToUpdate.bookTitle = title
-            bookd.updateBook(bookToUpdate)
-        }
+        return title
     }
 
     /**
      * Locate <div> with the class name "chapter".
      * Within that div, each one has a <h2> that represents their title,
      * and each one has <p> tags that represents all their content.
-     * Store the books chapters and their corresponding content.
+     * Extract the books chapters and their corresponding content.
      */
-    private suspend fun storeChapterAndContentData(bookId: Int, directory: File, doc: Document){
-        val chapters = doc.select("div.chapter")
+    private fun extractChapterAndContent(bookId: Int, directory: File, doc: Document)
+    :Pair<List<UiChapter>, List<UiContent>>{
+        val chapters = mutableListOf<UiChapter>()
+        val contents = mutableListOf<UiContent>()
+
+        val chapterDivs = doc.select("div.chapter")
         //might need to do smt here so that it doesnt save "toc" chapter
         var order = 1
-        for( c in chapters){
+        for( c in chapterDivs){
             val chapTitle = c.select("h2").text().trim()
             val contentp = c.select("p")
 
             val formattedContent = contentp.joinToString( "\n" ) {it.outerHtml()}
             saveChaptersInHtml(directory, order, chapTitle, formattedContent)
 
-            val chapter = Chapter(bookId, chapTitle, order, 0)
-            val chapId = chapterd.insertChapter(chapter)
-            val content = Content(chapId.toInt(), formattedContent)
-            val contentId = contentd.insertContent(content)
+            val uiChapter = UiChapter(null, chapTitle, order, bookId, null)
+            chapters.add(uiChapter)
 
-            //now that we have content id, add it to the created chapter
-            chapter.contentId = contentId.toInt()
-            chapterd.updateChapter(chapter)
+
+            val uiContent = UiContent(null, 0, formattedContent) // temporarily chapter id is set to 0
+            contents.add(uiContent)
 
             order++
         }
+        return Pair(chapters, contents)
     }
     /**
      * Save processed chapters as seperate HTML files
