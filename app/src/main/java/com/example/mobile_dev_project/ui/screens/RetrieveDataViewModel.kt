6e071b2f0a878806ebbaa7c77a3a1e9d0521a4ee
FrancisMignacca <francispatmignacca@yahoo.com>
2025-11-04 14:42:49 -0500

    Add retrieval functions to retrieve entities and transform them into Ui versions

---
+++ app/src/main/java/com/example/mobile_dev_project/ui/screens/RetrieveDataViewModel.kt
@@ -0,0 +1,37 @@
+package com.example.mobile_dev_project.ui.screens
+
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.mobile_dev_project.data.repository.*
+import com.example.mobile_dev_project.data.UiBook
+import com.example.mobile_dev_project.data.UiChapter
+import com.example.mobile_dev_project.data.UiContent
+import jakarta.inject.Inject
+import kotlinx.coroutines.launch
+import com.example.mobile_dev_project.data.mappers.*
+import com.example.mobile_dev_project.data.entity.*
+import kotlinx.coroutines.flow.first
+
+
+class RetrieveDataViewModel @Inject constructor(
+    private val bookRepository: BookRepository,
+    private val chapterRepository: ChapterRepository,
+    private val contentRepository: ContentRepository
+) : ViewModel() {
+    suspend fun getBookshelf() : List<UiBook>{
+        return bookRepository.allBooks.first().map { it.toUi() }
+    }
+    suspend fun getChaptersForBook(bookId: Int): List<UiChapter>?{
+        return chapterRepository.getChaptersForBook(bookId).first().map{ it.toUi()}
+    }
+    suspend fun getContentForChapter(chapId: Int): UiContent?{
+        val content = contentRepository.getContentForChapter(chapId)
+
+        return content.first()?.toUi()
+    }
+
+}
