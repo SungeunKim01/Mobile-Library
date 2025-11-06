d69a645bb1fc18fa279baec5e0e2785f7525d91d
Dorian Castillo <=>
2025-11-05 22:10:52 -0500

    Start test for position view model.

---
+++ app/src/test/java/com/example/mobile_dev_project/ui/screens/PositionViewModelTest.kt
@@ -0,0 +1,21 @@
+package com.example.mobile_dev_project.ui.screens
+
+
+import com.example.mobile_dev_project.data.repository.ContentRepository
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
+import org.mockito.kotlin.times
+import kotlin.test.assertEquals
+
+class PositionViewModelTest {
+  @Test
+  fun getScrollPosition_ReturnsCorrectValue() = runTest {
+   val contentId = 1
+   val pos = 0.75f
+
+  }
+}
