597831b8f84295d0982cfcfc3dafb143bf344460
FrancisMignacca <francispatmignacca@yahoo.com>
2025-11-05 21:24:29 -0500

    Fix uppercase lowercase issues

--- app/src/main/java/com/example/mobile_dev_project/nav/Route.kt
+++ app/src/main/java/com/example/mobile_dev_project/nav/Route.kt
@@ -3,9 +3,9 @@ package com.example.mobile_dev_project.nav
 sealed class Route(val route: String) {
     data object Download : Route("download")
     data object Search   : Route("search")
-    data object Home : Route("Home")
-    data object Content: Route("Content/{bookId}"){
-        fun createRoute(bookId: Int): String = "Content/$bookId"
+    data object Home : Route("home")
+    data object Content: Route("content/{bookId}"){
+        fun createRoute(bookId: Int): String = "content/$bookId"
     }
     data object Reading: Route("reading/{chapterIndex}"){
         fun createRoute(chapterIndex: Int): String = "reading/$chapterIndex"
