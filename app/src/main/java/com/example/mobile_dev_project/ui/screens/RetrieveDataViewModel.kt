1f75d018513015fa4060f234a222298241382bf2
Dorian Castillo <=>
2025-12-08 18:59:59 -0500

    Add fake view models.

--- app/src/main/java/com/example/mobile_dev_project/ui/screens/RetrieveDataViewModel.kt
+++ app/src/main/java/com/example/mobile_dev_project/ui/screens/RetrieveDataViewModel.kt
@@ -8,28 +8,30 @@ import com.example.mobile_dev_project.data.UiChapter
 import com.example.mobile_dev_project.data.UiContent
 import javax.inject.Inject
 import com.example.mobile_dev_project.data.mappers.*
+import com.example.mobile_dev_project.data.mockBook
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.map
 
 @HiltViewModel
-class RetrieveDataViewModel @Inject constructor(
-    private val bookRepository: BookRepository,
-    private val chapterRepository: ChapterRepository,
-    private val contentRepository: ContentRepository
+open class RetrieveDataViewModel @Inject constructor(
+    private val bookRepository: BookRepository?,
+    private val chapterRepository: ChapterRepository?,
+    private val contentRepository: ContentRepository?
 ) : ViewModel() {
-    fun getBookshelf(): Flow<List<UiBook>> {
-        return bookRepository.allBooks.map { books ->
+    open fun getBookshelf(): Flow<List<UiBook>> {
+        return bookRepository?.allBooks?.map { books ->
             books.map { it.toUi() }
-        }
+        } ?: flowOf(emptyList())
     }
-    fun getChaptersForBook(bookId: Int): Flow<List<UiChapter>> {
-        return chapterRepository.getChaptersForBook(bookId)
-            .map { chapters -> chapters.map { it.toUi() } }
+    open fun getChaptersForBook(bookId: Int): Flow<List<UiChapter>> {
+        return chapterRepository?.getChaptersForBook(bookId)
+            ?.map { chapters -> chapters.map { it.toUi() } } ?:flowOf(emptyList())
     }
-    fun getContentForChapter(chapId: Int): Flow<UiContent?> {
-        return contentRepository.getContentForChapter(chapId)
-            .map { contentEntity -> contentEntity?.toUi() }
+    open fun getContentForChapter(chapId: Int): Flow<UiContent?> {
+        return contentRepository?.getContentForChapter(chapId)
+            ?.map { contentEntity -> contentEntity?.toUi() } ?:flowOf(null)
     }
 
 }
