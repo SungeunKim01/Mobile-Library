e6b76aa56a257708ecb112fa764e60032107f857
Dorian Castillo <=>
2025-12-08 21:22:53 -0500

    Move navigation testing to same file as kt.

--- app/src/androidTest/java/com/example/mobile_dev_project/ui/screens/ReadingScreenKtTest.kt
+++ app/src/androidTest/java/com/example/mobile_dev_project/ui/screens/ReadingScreenKtTest.kt
@@ -3,7 +3,9 @@ package com.example.mobile_dev_project.ui.screens
 
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onAllNodesWithText
 import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.mobile_dev_project.ui.theme.MobileDevProjectTheme
@@ -21,16 +23,70 @@ class ReadingScreenKtTest {
     @get: Rule
     val composeTestRule = createComposeRule()
     private lateinit var fakeTTsVM: FakeTTsViewModel
+    private lateinit var fakePosVM: FakePositionViewModel
 
     /**
-     * Set up the compose rule with the ReadingScreen composable
+     * Set up the compose rule with the ReadingScreen fake view models
      */
     @Before
     fun setUp() {
-        val fakePosVM = FakePositionViewModel()
-        val fakeRetrieveVM = FakeRetrieveDataViewModel(mockChapters, mockContents)
         fakeTTsVM = FakeTTsViewModel()
+        fakePosVM = FakePositionViewModel()
 
+    }
+
+    /**
+     * Test that the search button is displayed and that it is clickable
+     */
+    @Test
+    fun searchButtonIsDisplayed(){
+        var clicked = false
+        composeTestRule.setContent {
+            MobileDevProjectTheme {
+                ReadingScreenForTest(
+                    chapters = mockChapters,
+                    contents = mockContents,
+                    chapterIndexSelected = 0,
+                    ttsVM = fakeTTsVM,
+                    posVM = fakePosVM,
+                    onSearch = { clicked = true } // capture click
+                )
+            }
+        }
+        composeTestRule.onNodeWithTag("search_btn").assertExists().assertIsDisplayed()
+        composeTestRule.onNodeWithTag("search_btn").performClick()
+
+        assert(clicked)
+    }
+
+    /**
+     * Test that the back button is displayed and that it is clickable
+     */
+    @Test
+    fun backButtonIsDisplayed(){
+        var clickedBack = false
+
+        composeTestRule.setContent {
+            ReadingScreenForTest(
+                chapters = mockChapters,
+                contents = mockContents,
+                chapterIndexSelected = 0,
+                ttsVM = FakeTTsViewModel(),
+                posVM = FakePositionViewModel(),
+                onBack = { clickedBack = true }
+            )
+        }
+
+        composeTestRule.onNodeWithTag("back_btn").assertIsDisplayed()
+        composeTestRule.onNodeWithTag("back_btn").performClick()
+        assert(clickedBack)
+    }
+
+    /**
+     * Test that the chapter title is displayed
+     */
+    @Test
+    fun chapterTitleIsDisplayed(){
         composeTestRule.setContent {
             MobileDevProjectTheme {
                 ReadingScreenForTest(
@@ -42,32 +98,6 @@ class ReadingScreenKtTest {
                 )
             }
         }
-    }
-
-    /**
-     * Test that the search button is displayed and that it is clickable
-     */
-    @Test
-    fun searchButtonIsDisplayed(){
-        composeTestRule.onNodeWithTag("search_btn").assertExists().assertIsDisplayed()
-        composeTestRule.onNodeWithTag("search_btn").performClick()
-
-    }
-
-    /**
-     * Test that the back button is displayed and that it is clickable
-     */
-    @Test
-    fun backButtonIsDisplayed(){
-        composeTestRule.onNodeWithTag("back_btn").assertExists().assertIsDisplayed()
-        composeTestRule.onNodeWithTag("back_btn").performClick()
-    }
-
-    /**
-     * Test that the chapter title is displayed
-     */
-    @Test
-    fun chapterTitleIsDisplayed(){
         composeTestRule.onNodeWithTag("title", useUnmergedTree = true).assertExists().assertIsDisplayed()
 
     }
@@ -77,6 +107,17 @@ class ReadingScreenKtTest {
      */
     @Test
     fun chapterContentIsDisplayed(){
+        composeTestRule.setContent {
+            MobileDevProjectTheme {
+                ReadingScreenForTest(
+                    chapters = mockChapters,
+                    contents = mockContents,
+                    chapterIndexSelected = 0,
+                    ttsVM = fakeTTsVM,
+                    posVM = fakePosVM
+                )
+            }
+        }
         composeTestRule.onNodeWithTag("content", useUnmergedTree = true).assertExists().assertIsDisplayed()
     }
     /**
@@ -84,6 +125,17 @@ class ReadingScreenKtTest {
      */
     @Test
     fun ttsBarIsDisplayed(){
+        composeTestRule.setContent {
+            MobileDevProjectTheme {
+                ReadingScreenForTest(
+                    chapters = mockChapters,
+                    contents = mockContents,
+                    chapterIndexSelected = 0,
+                    ttsVM = fakeTTsVM,
+                    posVM = fakePosVM
+                )
+            }
+        }
         composeTestRule.onNodeWithTag("tts_bar", useUnmergedTree = true).assertExists().assertIsDisplayed()
     }
 
